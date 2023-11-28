6c6b86867c285f3ff388171fd37de782e80bd355
chen-kaminski <chen.kaminski@payme.io>
2023-11-28 15:23:34 +0200

    html test for pwa

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -12,14 +12,14 @@ const BottomBar = () => {
           <Link
             to={link.route}
             key={link.label}
-            className={`${isActive && 'bg-primary-500 rounded-[10px]'} flex-center flex-col  gap-1 p-2 transition`}
+            className={`${isActive && 'background-color:yellow rounded-[10px]'} flex-center flex-col  gap-1 p-2 transition`}
           >
             <img src={link.imgURL} alt={link.label}
               className={`${isActive && 'invert-white'}`}
               width={16}
               height={16}
             />
-            <p className="tiny-medium text-align-2">{link.label}</p>
+            <p className="tiny-medium text-align-2 background-color:yellow">{link.label}</p>
           </Link>
 
         )
--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -14,7 +14,7 @@ const PostCard = ({post} : PostTypeProps) => {
     if(!post.creator) return ;
 
   return (
-    <div className="post-card">
+    <li className="post-card">
         <div className="flex-between">
             <div className="flex items-center gap-3">
                 <Link to={`/profile/${post.ownerId}`}>
@@ -63,7 +63,7 @@ const PostCard = ({post} : PostTypeProps) => {
             </Link>
 
             <PostStats post={post}  userId={user.id || ''}/>
-    </div>
+    </li>
   )
 }
 
