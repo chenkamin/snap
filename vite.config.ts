d5def5626f8b9ded49eee8c1471382b15e7d8abc
chen-kaminski <chen.kaminski@payme.io>
2023-11-28 21:29:34 +0200

    pwa test

--- vite.config.ts
+++ vite.config.ts
@@ -9,42 +9,21 @@ export default defineConfig({
   plugins: [react(),VitePWA({
     registerType: 'autoUpdate', // Choose 'prompt' or 'autoUpdate' based on your preference
     includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    manifest:{
-      name:"React-vite-app",
-      short_name:"react-vite-app",
-      description:"I am a simple vite app",
-      icons:[{
-        src: '/android-chrome-192x192.png',
-        sizes:'192x192',
-        type:'image/png',
-        purpose:'favicon'
-      },
-      {
-        src:'/android-chrome-512x512.png',
-        sizes:'512x512',
-        type:'image/png',
-        purpose:'favicon'
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes:'180x180',
-        type:'image/png',
-        purpose:'apple touch icon',
-      },
-      {
-        src: '/maskable_icon.png',
-        sizes:'512x512',
-        type:'image/png',
-        purpose:'any maskable',
-      }
-    ],
-    theme_color:'#171717',
-    background_color:'#f0e7db',
-    display:"standalone",
-    scope:'/',
-    start_url:"/",
-    orientation:'portrait'
-    }
+    manifest: {
+      name: 'Your App Name',
+      short_name: 'App',
+      description: 'Your Progressive Web App Description',
+      icons: [
+        {
+          src: 'assets/images/profile.png',
+          "sizes": "512x512",
+          type: 'image/png',
+          purpose: 'maskable any',
+        },
+      ],
+      theme_color: '#ffffff',
+      // ... other manifest properties
+    },
   }),
 ],
   resolve: {
