68b1bbf95b52a77b2bc8d28b189d98c59f71b2ce
chen-kaminski <chen.kaminski@payme.io>
2023-11-28 15:10:27 +0200

    pwa test

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { resolve } from 'path'
+import { VitePWA } from "vite-plugin-pwa";
 
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA({
+    registerType: 'autoUpdate', // Choose 'prompt' or 'autoUpdate' based on your preference
+    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Your App Name',
+      short_name: 'App',
+      description: 'Your Progressive Web App Description',
+      icons: [
+        {
+          src: '/assets/icon-512x512.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable any',
+        },
+      ],
+      theme_color: '#ffffff',
+      // ... other manifest properties
+    },
+  }),
+],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
   },
